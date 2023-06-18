5e641d945868b3864eb63d023b71b9950d6c78e2
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-06-18 13:31:42 -0300

    feat: create route for EventsComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventsComponent } from './events/events.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot([{ path: 'event', component: EventsComponent }]),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
