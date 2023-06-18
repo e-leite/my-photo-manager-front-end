36dd3c05353a9fc0c381a7410e1a3352ef49c79d
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-06-18 13:18:14 -0300

    feat: add MatInput MatFormField MatPaginator MatTable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { EventsComponent } from './events/events.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, EventsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatTableModule,
     MatToolbarModule,
   ],
   providers: [],
