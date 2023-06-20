3a4c1d5429051cf56d51a3129b359f0c2ac6d672
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-06-19 21:37:51 -0300

    feat: create getStates and getCitiesByState in IbgeApiService

--- src/app/shared/ibge-api.service.ts
+++ src/app/shared/ibge-api.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, take } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class IbgeApiService {
+  constructor(private httpCliente: HttpClient) {}
 
-  constructor() { }
+  getStates(): Observable<{}> {
+    return this.httpCliente
+      .get(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome`
+      )
+      .pipe(take(1));
+  }
+
+  getCitiesByState(uf: string): Observable<{}> {
+    return this.httpCliente
+      .get(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
+      )
+      .pipe(take(1));
+  }
 }
