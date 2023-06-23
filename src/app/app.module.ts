b99c8ed0c76ca5c4802a474692b0d31be26c3d8f
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-06-23 06:55:14 -0300

    feat: create MaterialModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatInputModule } from '@angular/material/input';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { EventsTableComponent } from './events-table/events-table.component';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { EventsFormComponent } from './events-form/events-form.component';
-import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
-import { MatSelectModule } from '@angular/material/select';
+
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+
 import { IbgeApiService } from './shared/ibge-api.service';
 import { EventsService } from './shared/events.service';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [AppComponent, EventsTableComponent, EventsFormComponent],
@@ -32,17 +24,7 @@ import { EventsService } from './shared/events.service';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MatButtonModule,
-    MatDatepickerModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    MatNativeDateModule,
-    MatPaginatorModule,
-    MatSelectModule,
-    MatTableModule,
-    MatToolbarModule,
+    MaterialModule,
   ],
   providers: [
     { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material/core';
+
+const MODULES = [
+  MatButtonModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatSelectModule,
+  MatTableModule,
+  MatToolbarModule,
+];
+
+@NgModule({
+  declarations: [],
+  imports: [MODULES],
+  exports: [MODULES],
+})
+export class MaterialModule {}
